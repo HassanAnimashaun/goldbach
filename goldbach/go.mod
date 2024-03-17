5841f8783e08418b3762701886ec5ab8f654a9be
HassanAnimashaun <130189668+HassanAnimashaun@users.noreply.github.com>
2024-03-17 10:48:36 -0400

    Add files via upload
    Template for Goldbach for Go

---
+++ goldbach/go.mod
@@ -0,0 +1,3 @@
+module example/goldbach
+
+go 1.22.1
---
+++ goldbach/goldbach.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func goldbach() {
+	fmt.Println("Hello, World!")
+}
+func main() {
+	goldbach() // Call the goldbach function
+}
